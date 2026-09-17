49294c8a8b7c56f6bee43e5410e7a52779e71617
Virakboth Chharn <virakbothchharn6@gmail.com>
2026-09-17 13:00:38 +0700

    chore(frontend): configure vite server polling and axios base URL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-import App from './App.vue'
-import router from './router'
+// note bash to laravel backen
+axios.defaults.baseURL = "http://localhost/api";
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
